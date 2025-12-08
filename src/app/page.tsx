ec0c53875b6e14d9d16fdd442a38aa3e66a7a2e1
laurhakob <laurhakob@gmail.com>
2025-12-08 22:18:07 +0400

    database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-const Page = () => {
-  return (
-    <div className="text-red-500 font-bold">Hello world</div>
-  )
-}
+import prisma from "@/lib/db";
 
-export default Page
+const Page = async () => {
+  const users = await prisma.user.findMany();
+
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      {JSON.stringify(users)}
+    </div>
+  );
+};
+
+export default Page;
