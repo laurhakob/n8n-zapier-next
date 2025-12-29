96e524b4f971337d750f8928b0f93d256f4e4e4c
laurhakob <laurhakob@gmail.com>
2025-12-30 03:34:26 +0400

    sidebar layout

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  devIndicators: false,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/workflows",
+        permanent: false,
+      },
+    ];
+  },
   /* config options here */
 };
 
