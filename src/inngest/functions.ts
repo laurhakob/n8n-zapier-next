76011b235e77bdc9610e4ce63a7c7de49aa7ed08
laurhakob <laurhakob@gmail.com>
2025-12-30 00:29:52 +0400

    ai provider

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -117,11 +117,12 @@ import { inngest } from "./client";
 //   },
 // );
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+export const execute = inngest.createFunction(
+  { id: "execute-ai" },
+  { event: "execute/ai" },
   async ({ event, step }) => {
     await step.sleep("wait-a-moment", "1s");
     return { message: `Hello ${event.data.email}` };
   }
 );
+ 
