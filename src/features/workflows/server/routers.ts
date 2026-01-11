50a7825b0a9a66bba5a2f50e1ee9651c31814649
laurhakob <laurhakob@gmail.com>
2026-01-12 01:33:47 +0400

    workflow page

--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -129,7 +129,7 @@ export const workflowsRouter = createTRPCRouter({
    getOne: protectedProcedure
      .input(z.object({ id: z.string() }))
     .query(async ({ ctx, input }) => {
-        return prisma.workflow.findUnique({
+        return prisma.workflow.findUniqueOrThrow({
     //   const workflow = await prisma.workflow.findUniqueOrThrow({
         where: { id: input.id, userId: ctx.auth.user.id }
      //  include: { nodes: true, connections: true },
